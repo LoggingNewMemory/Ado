14668ff22147822e399c199135cee635dbac0a61
KanagawaYamada <yamadakernel.project@gmail.com>
2026-02-08 15:16:33 +0700

    Add more tweaks
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- Modules/AdoKang/OdoruPonpokorin.sh
+++ Modules/AdoKang/OdoruPonpokorin.sh
@@ -1,5 +1,6 @@
 settings put global audio_safe_volume_state 0
-
 settings put global bluetooth_a2dp_supports_optional_codecs_enabled 1
-
+settings put global bluetooth_a2dp_src_codec_priority 1000000
 resetprop ro.audio.flinger_standby_time_ms 3000
+resetprop ro.audio.monitorRotation true
+resetprop vendor.audio.offload.buffer.size.kb 64
