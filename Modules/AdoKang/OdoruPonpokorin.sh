1ee00f474fd64af04a70734dccd3b4953cfce139
KanagawaYamada <yamadakernel.project@gmail.com>
2026-02-11 15:53:36 +0700

    Move all props to system props and add Renice (Renice will be removed if causes problems)
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- Modules/AdoKang/OdoruPonpokorin.sh
+++ Modules/AdoKang/OdoruPonpokorin.sh
@@ -2,6 +2,6 @@
 settings put global audio_safe_volume_state 0
 settings put global bluetooth_a2dp_supports_optional_codecs_enabled 1
 settings put global bluetooth_a2dp_src_codec_priority 1000000
-resetprop ro.audio.flinger_standby_time_ms 3000
-resetprop vendor.audio.offload.buffer.size.kb 256
-resetprop ro.audio.monitorRotation true
+
+for pid in $(pidof audioserver); do renice -n -20 -p $pid; done
+for pid in $(pidof com.android.bluetooth); do renice -n -20 -p $pid; done
