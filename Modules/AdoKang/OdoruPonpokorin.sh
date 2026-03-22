543e9d129b5cb0794a1e7485ec3e8ba8bf8ef8d1
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-03-22 16:16:59 +0700

    Add ro.af.client_heap_size_kbyte, ro.audio.spatializer_enabled. Add Real-Time Scheduling (chrt) and expand hardware targets to renice
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- Modules/AdoKang/OdoruPonpokorin.sh
+++ Modules/AdoKang/OdoruPonpokorin.sh
@@ -3,5 +3,17 @@ settings put global audio_safe_volume_state 0
 settings put global bluetooth_a2dp_supports_optional_codecs_enabled 1
 settings put global bluetooth_a2dp_src_codec_priority 1000000
 
-for pid in $(pidof audioserver); do renice -n -20 -p $pid; done
-for pid in $(pidof com.android.bluetooth); do renice -n -20 -p $pid; done
+for pid in $(pidof audioserver); do
+    renice -n -20 -p $pid
+    chrt -f -p 99 $pid
+done
+
+for pid in $(pidof com.android.bluetooth); do
+    renice -n -20 -p $pid
+    chrt -f -p 99 $pid
+done
+
+for pid in $(pidof android.hardware.audio.service android.hardware.audio.service.mediatek android.hardware.audio@7.1-service android.hardware.audio@7.0-service android.hardware.audio@6.0-service android.hardware.audio@5.0-service android.hardware.audio@4.0-service android.hardware.audio@2.0-service); do
+    renice -n -20 -p $pid
+    chrt -f -p 99 $pid
+done
